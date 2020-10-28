27acef22a6e44393b82d20f6e9728b58997de5b2
Itzel01 <itzel.areymi.rendon@gmail.com>
2020-10-28 20:32:42 +0000

    stuck on 4

--- answers.js
+++ answers.js
@@ -18,14 +18,32 @@ function combineArrays() {
 }
 
 // Question 3
-function arrayPlusArray() {
-  // Write your code here
-}
+function arrayPlusArray(arr1, arr2, ...more){
+  //variable for concatenated arrayPlusArray
+  let newArray = arr1.concat(arr2, ...more); 
+  //variable that will hold the values of the new array
+  let sum = 0;
+  //for loop will iterate through the new array.
+  for(let i = 0; i < newArray.length; i++){
+    //Will reAdd the value for each index so it gets summed automatically.
+    sum += newArray[i];
+  }
+  return sum;
+};
+
+arrayPlusArray([1, 2, 4], [4, 5, 6])
 
 // Question 4
-function abbrevName() {
+function abbrevName(name) {
   //Write your code here
+  let initial = '';
+  for(let i = 0; i < name.length; i++){
+    if (name[i] === name.toUpperCase()){
+      initial += name[i];
+    }
+  }
 }
+abbrevName('Sam Sam');
 
 // Question 5
 function makeTitle() {
